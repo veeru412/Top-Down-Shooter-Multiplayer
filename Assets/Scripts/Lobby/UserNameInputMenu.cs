4f65b8e4c42c6efbd92c81e8d6d1a1097530fa3c
veeru412 <verababu00@gmail.com>
2025-08-03 07:01:43 +0530

    Lobby creation done

---
+++ Assets/Scripts/Lobby/UserNameInputMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Assets.Scripts.Lobby
+{
+  public class UserNameInputMenu : MonoBehaviour
+  {
+    [SerializeField] private TMP_InputField usernameInputField;
+    [SerializeField] private Button submitButton;
+
+    public static string PlayerUsername { get; private set; }
+
+    private void Start()
+    {
+      if (PlayerPrefs.HasKey("Username"))
+      {
+        string savedName = PlayerPrefs.GetString("Username");
+        usernameInputField.text = savedName;
+        PlayerUsername = savedName;
+        gameObject.SetActive(false);
+      }
+
+      submitButton.onClick.AddListener(OnSubmit);
+    }
+
+    private void OnSubmit()
+    {
+      string username = usernameInputField.text.Trim();
+
+      if (string.IsNullOrEmpty(username))
+      {
+        Debug.LogWarning("Username is empty.");
+        return;
+      }
+
+      PlayerUsername = username;
+      PlayerPrefs.SetString("Username", username);
+      PlayerPrefs.Save();
+
+      Debug.Log("Username saved: " + PlayerUsername);
+
+    }
+  }
+}
